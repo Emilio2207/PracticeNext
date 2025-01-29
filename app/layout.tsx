c4db4ac07b2635ca5f9baf9695f827b4d9f42bfc
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-01-29 17:21:15 -0300

    styles commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import "./ui/global.css";
+
+//------------------------------------------------------------------------
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <h1>Este Titulo</h1>
-        <div style={{ border: "1px solid #000" }}>{children}</div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
